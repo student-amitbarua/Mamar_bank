a18439658de5711667a2ec3d261910d04a1f20dd
Amit Barua <amitbarua737@gmail.com>
2025-10-02 19:27:46 +0600

    changes done

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -38,6 +38,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-h8w7.onrender.com', 'https://*.127.0.0.1']
 
 
 # Application definition
